32d9d83b54695b4cb50db3c0cc15d22d4a432d00
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-20 22:38:40 +0530

    add: added yamk config file and chain link imports

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,9 +2,19 @@
 
 pragma solidity ^0.6.6;
 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
 contract Lottery {
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFee;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18); // storing the entry whenever  we initilized an instance of the contract
+        ethUsdPriceFee = AggregatorV3Interface(_priceFeedAddress);
+    }
+
     // entering a lottery
-    function enter() public {}
+    function enter() public payable {}
 
     // entering a getting the entrance fee
     function getEntranceFee() public {}
