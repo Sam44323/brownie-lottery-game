7f00bbcc7e4128b9958ddefeaf264c47a65c2307
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-20 22:13:56 +0530

    add: added the basic contract skeletal

---
+++ contracts/Lottery.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+contract Lottery {
+    // entering a lottery
+    function enter() public {}
+
+    // entering a getting the entrance fee
+    function getEntranceFee() public {}
+
+    // method for starting a lottery(admin)
+    function startLottery() public {}
+
+    // method for ending a lottery(admin)
+    function endLottery() public {}
+}
