a611d5d71b54d243f20773bc8a5f354bcce9889c
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-22 23:24:24 +0530

    add: added params for get account function for scripts

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -4,7 +4,11 @@ LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 
 
-def get_account():
-    if(network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS):
+def get_account(index=None, id=None):
+    if id:
+        return accounts.load(index)
+    elif index:
+        return accounts[index]
+    elif(network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS):
         return accounts[0]
     return accounts.add(config["wallets"]["from_key"])
