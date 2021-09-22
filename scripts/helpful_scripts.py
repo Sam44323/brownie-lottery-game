62e11bcc99fcc8abeef2c0486bdd91b1416f8e08
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-22 22:54:49 +0530

    add: added environments array for the get accounts

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,5 +1,11 @@
-from brownie import accounts, network
+from brownie import accounts, network, config
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 
 
 def get_account():
-    return accounts[0]
+    if(network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
