824c198a129c5a4a144fea66816936477d67517f
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-23 21:36:35 +0530

    add: added vrf coordinator and price feed addresses for rinkeby

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -4,7 +4,7 @@ from scripts.helpful_scripts import get_account, get_contract
 
 def deploy_lottery():
     account = get_account()
-    lottery = Lottery.deploy(get_contract("eth_usd_price_feed"))
+    lottery = Lottery.deploy(get_contract("eth_usd_price_feed").address)
 
 
 def main():
