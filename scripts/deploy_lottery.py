92c6d0020fc67b2d4b1af3c1f69394dc58a0d7ec
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-23 21:38:30 +0530

    add: added method trigger for vrf and price feed for testnet

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,10 +1,15 @@
-from brownie import Lottery
+from brownie import Lottery, network, config
 from scripts.helpful_scripts import get_account, get_contract
 
 
 def deploy_lottery():
     account = get_account()
-    lottery = Lottery.deploy(get_contract("eth_usd_price_feed").address)
+    lottery = Lottery.deploy(
+        get_contract(
+            "eth_usd_price_feed").address,
+        config["networks"][network.show_active()]["vrf_coordinator"],
+        config["networks"][network.show_active()]["eth_usd_price_feed"],
+    )
 
 
 def main():
