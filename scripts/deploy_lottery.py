f124543c78d5b87a82416ac56ee6e79921be3666
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-23 21:31:44 +0530

    update: updated the get contract method call for the lottery

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -4,7 +4,7 @@ from scripts.helpful_scripts import get_account, get_contract
 
 def deploy_lottery():
     account = get_account()
-    lottery = Lottery.deploy(get_contract())
+    lottery = Lottery.deploy(get_contract("eth_usd_price_feed"))
 
 
 def main():
