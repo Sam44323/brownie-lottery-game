f9aab8b4e9fa1faedf050cd2c55994c852550b93
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-26 19:15:46 +0530

    add: added test for getEntranceFee method in contract

--- tests/unit/test_lottery_unit.py
+++ tests/unit/test_lottery_unit.py
@@ -1,7 +1,12 @@
 from brownie import Lottery, network, config
 from scripts.helpful_scripts import get_account, get_contract, fund_with_links
+from scripts.deploy_lottery import deploy_lottery
+from web3 import Web3
 import time
 
 
 def test_get_entrance_fee():
-    pass
+    lottery = deploy_lottery()
+    expected_entrance_fee = Web3.toWei(0.025, 'ether')
+    entranceFee = lottery.getEntranceFee()
+    assert expected_entrance_fee == entranceFee
