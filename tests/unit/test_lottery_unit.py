d0743d6e0e625204a5388e48a94ae16aa679a6a7
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-26 19:38:44 +0530

    add: added skipping in testing

--- tests/unit/test_lottery_unit.py
+++ tests/unit/test_lottery_unit.py
@@ -1,9 +1,14 @@
+from brownie import network
 from scripts.deploy_lottery import deploy_lottery
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from web3 import Web3
 import time
+import pytest
 
 
 def test_get_entrance_fee():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
     lottery = deploy_lottery()
     expected_entrance_fee = Web3.toWei(0.025, 'ether')
     entranceFee = lottery.getEntranceFee()
