67d161c4047ba1775152a0612f3b50c52f7a33ab
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-26 23:13:46 +0530

    add: added integ testing for lotter

--- tests/integration/test_lottery_integration.py
+++ tests/integration/test_lottery_integration.py
@@ -1,3 +1,20 @@
-from brownie import Lottery, network, config
-from scripts.helpful_scripts import get_account, get_contract, fund_with_links
+from scripts.deploy_lottery import deploy_lottery
 import time
+import pytest
+from brownie import network
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account, fund_with_links
+
+
+def test_can_pick_winner():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    fund_with_links(lottery)
+    lottery.endLottery({"from": account})
+    time.sleep(60)
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
